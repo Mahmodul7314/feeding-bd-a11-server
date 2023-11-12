4ccfff5ec455b43df736141e2fcf111caf59f3e5
Mahmodul7314 <mahmudulhasan7314@gmail.com>
2023-11-12 04:38:02 -0800

    update for vercel deploy

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000
 
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-//user: feedingBD
-//Pass: M4wTAcxT1Ggxt1Xs
 
-console.log(process.env.DB_USER)
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b2stcle.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 
     const foodCollection = client.db('feedingBD').collection('foods');
